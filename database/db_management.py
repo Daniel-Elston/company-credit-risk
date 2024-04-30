924c59bdf349781612f3d0c7e3b0b3e800ba8309
Daniel-Elston <delstonds@outlook.com>
2024-04-30 09:58:45 +0100

    Insert Data to DB
    - db_ops.py created to handle authentication and connections
    - db_management.py created to insert data
    - DataBaseManagement object and insert_data method created

--- database/db_management.py
+++ database/db_management.py
@@ -1,30 +1,37 @@
-import pandas as pd
+from __future__ import annotations
+
 from pathlib import Path
 
-from psycopg2 import pool
+import pandas as pd
+
 from database.db_ops import DataBaseOps
+# from psycopg2 import pool
 creds, pg_pool, engine, conn = DataBaseOps().ops_pipeline()
+
 
 class DataBaseManagement(DataBaseOps):
     def __init__(self):
         super().__init__()
         self.data_path = Path(self.config['data_path'])
-        
+
     def insert_data(self):
         self.logger.info('Begininning data insert')
         raw_data = pd.ExcelFile(self.data_path, engine='openpyxl')
-        
+
         count = 0
         try:
             for sheet_name in raw_data.sheet_names:
                 count += 1
                 df = pd.read_excel(raw_data, sheet_name=sheet_name)
-                df.to_sql(sheet_name.lower(), con=engine, index=False, if_exists='replace')
-                self.logger.info('Inserted sheet %s: %s of %s', sheet_name, count, len(raw_data.sheet_names))
+                df.to_sql(sheet_name.lower(), con=engine,
+                          index=False, if_exists='replace')
+                self.logger.info(
+                    'Inserted sheet %s: %s of %s', sheet_name, count, len(raw_data.sheet_names))
 
         finally:
             raw_data.close()
-            
+
+
 if __name__ == '__main__':
     db = DataBaseManagement()
     try:
